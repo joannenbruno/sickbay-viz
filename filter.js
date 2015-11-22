bd69cea3308319ec4aa7a6030f21f8ff5d15726b
abbottmb12 <abbott_b@tamu.edu>
2015-11-21 20:02:24 -0600

    created filter.js

---
+++ filter.js
@@ -0,0 +1,21 @@
+// set the global variable
+var Data;
+var reducedData= {data:[]};
+
+$.getJSON('http://joannenbruno.github.io/sickbay-viz/health.json', function(data){
+	Data = data;
+	//reducedData = Data.data[1];
+	//[17] for total cost column
+	//40170
+	//alert(Data.data[40169][17]);
+	var highestCost = 0;
+	for (var i=0,  tot=Data.data.length; i < tot; i++) {
+		if (parseInt(Data.data[i][17]) > parseInt(highestCost) ){
+			console.log(Data.data[i][17] + ">?" + highestCost);
+			reducedData.data = Data.data[i];
+			highestCost = reducedData.data[17];
+		}
+	}
+	$('#content').html(JSON.stringify(reducedData));
+});
+
