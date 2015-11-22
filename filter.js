b6b32c2a4ca5507d35b3380a49273de2f627e301
abbottmb12 <abbottmb12@gmail.com>
2015-11-21 23:48:17 -0600

    Changed to a file download in readable format

--- filter.js
+++ filter.js
@@ -2,21 +2,24 @@
 var Data;
 var reducedData= {highestCosting:[]};
 
-$.getJSON('http://abbottmb12.github.io/health.json', function(data){
-	Data = data;
-	//reducedData = Data.data[1];
-	//[17] for total cost column
-	//40170
-	//alert(Data.data[40169][17]);
-	var highestCost = 0;
-	for (var i=0,  tot=Data.data.length; i < tot; i++) {
-		if (parseInt(Data.data[i][17]) > parseInt(highestCost) ){
-			console.log(Data.data[i][17] + ">?" + highestCost);
-			reducedData.highestCosting[0] = Data.data[i];
-			highestCost = reducedData.highestCosting[0][17];
+
+function download() {
+	$.getJSON('http://abbottmb12.github.io/health.json', function(data){
+		Data = data;
+		//reducedData = Data.data[1];
+		//[17] for total cost column
+		//40170
+		//alert(Data.data[40169][17]);
+		var highestCost = 0;
+		for (var i=0,  tot=Data.data.length; i < tot; i++) {
+			if (parseInt(Data.data[i][17]) > parseInt(highestCost) ){
+				console.log(Data.data[i][17] + ">?" + highestCost);
+				reducedData.highestCosting[0] = Data.data[i];
+				highestCost = reducedData.highestCosting[0][17];
+			}
 		}
-	}
-	$('h3').hide();
-	$('textarea').show();
-	$('textarea').html(JSON.stringify(reducedData, null, 4));
-});
+		var stringified = JSON.stringify(Data, null, 4);
+		var blob = new Blob([stringified], {'type': 'application/json'});
+		saveAs(blob, "output.json");
+	});
+}
