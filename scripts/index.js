86ea06a85034e727c5ae8f370c7cf4bbb1daeab6
abbottmb12 <abbottmb12@gmail.com>
2015-12-13 12:19:05 -0600

    test

--- scripts/index.js
+++ scripts/index.js
@@ -40,6 +40,7 @@ function populateServiceDropdown(){
 }
 
 $(document).ready(function($){
+	//state selector
 	var stateList = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"];
 	var states = '';
 	for(var i=0; i<stateList.length; i++){
